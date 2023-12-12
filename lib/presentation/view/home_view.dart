3283308256e61767e9799dd255574769050a1ae4
mohamedSeiam <midoredase@gmail.com>
2023-12-12 20:42:57 +0200

    -added images to assets -added sounds -added fonts

--- lib/presentation/view/home_view.dart
+++ lib/presentation/view/home_view.dart
@@ -10,6 +10,6 @@ class HomeView extends StatefulWidget {
 class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return const Scaffold();
   }
 }
